d33c5850b6af4233a929c41a1a88571fc7856e86
Allen Ramirez <allenram.15@yahoo.com>
2026-04-21 09:55:53 -0400

    too much changes

--- crewmates/src/App.jsx
+++ crewmates/src/App.jsx
@@ -4,10 +4,18 @@ import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
-
+  
   return (
     <>
-      
+      <h1>Dragon Farm</h1>
+      <h4>Create your dream dragon squad</h4>
+
+      <div className="dragon-container">
+        <h3>Your Dragon Squad</h3>
+        <div className="dragon-list">
+          {/*Your dragon squad will appear here*/}
+        </div>
+      </div>
     </>
   )
 }
